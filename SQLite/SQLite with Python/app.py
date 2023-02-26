b0e2b4009bf0c57e22eb794b5987589ef29825a2
ahmad soliman <ahmad.suliman94@hotmail.com>
2023-02-26 20:49:47 +0100

    how to use sqlite on python

--- SQLite/SQLite with Python/app.py
+++ SQLite/SQLite with Python/app.py
@@ -2,81 +2,135 @@ import sqlite3
 from pprint import pprint
 
 
-# 1. Create a new database and connect to it
-conn = sqlite3.connect('./customer.db')
+class Database:
+    def __init__(self, db_file) -> None:
 
-# 2. cursor
-cursor = conn.cursor() 
+        ''' 
+        1. create connections to the database.
+        2. create the crusor
+        3. create the customer, product, and orders tables.
+        '''
+        self.conn  = sqlite3.connect(db_file)
+        self.cursor = self.conn.cursor()
+        self.create_tables()
+    
 
+    def create_tables(self):
 
-'''3. SQL Statments:
-- Create the customer table. 
-- Create the product table.
-- Create the orders table.
-''' 
+        '''3.  cretae SQL Statments then  then excute them:
+            - to create the customer table. 
+            - to create the product table.
+            - to create the orders table.
+        ''' 
 
-customer = '''CREATE TABLE customer (
-    customer_id INTEGER PRIMARY KEY,
-    first_name TEXT,
-    last_name TEXT
-);'''
+        customer = '''CREATE TABLE customer (
+            customer_id INTEGER PRIMARY KEY,
+            first_name TEXT,
+            last_name TEXT
+        );'''
 
-product = '''CREATE TABLE product (
-    product_id INTEGER PRIMARY KEY,
-    product_name TEXT,
-    product_price REAL
-);'''
+        product = '''CREATE TABLE product (
+            product_id INTEGER PRIMARY KEY,
+            product_name TEXT,
+            product_price REAL
+        );'''
 
-orders = '''CREATE TABLE orders (
-    order_id INTEGER PRIMARY KEY,
-    customer_id INTEGER,
-    product_id INTEGER,
-    order_quantity INTEGER,
-    FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
-    FOREIGN KEY (product_id) REFERENCES product(product_id)
-);'''
+        orders = '''CREATE TABLE orders (
+            order_id INTEGER PRIMARY KEY,
+            customer_id INTEGER,
+            product_id INTEGER,
+            order_quantity INTEGER,
+            FOREIGN KEY (customer_id) REFERENCES customer(customer_id),
+            FOREIGN KEY (product_id) REFERENCES product(product_id)
+        );'''
 
+        self.cursor.execute(customer)
+        self.cursor.execute(product)
+        self.cursor.execute(orders)
+    
+    def add_customers(self, first_name, last_name):
 
-# 4. Execute the SQL statments to create tables
-cursor.execute(customer) 
-cursor.execute(product)
-cursor.execute(orders)
+        '''
+        to insert values into customer table then commit the changes to the database
+        '''
 
+        self.cursor.execute("INSERT INTO customer (first_name, last_name) VALUES (?, ?)", (first_name, last_name))
+        self.conn.commit()
 
-# 5. Insert some sample data into the tables.
+    
+    def add_products(self, product_name, product_price):
 
-cursor.execute("INSERT INTO customer (first_name, last_name) VALUES (?, ?)", ('John ', 'Doe'))
-cursor.execute("INSERT INTO customer (first_name, last_name) VALUES (?, ?)", ('Ahmad', 'Soliman'))
-cursor.execute("INSERT INTO customer (first_name, last_name) VALUES (?, ?)", ('Muster', 'Mann'))
+        '''
+        to insert values into product table then commit the changes to the database
+        '''
 
+        self.cursor.execute("INSERT INTO product (product_name, product_price) VALUES (?, ?)", (product_name, product_price))
+        self.conn.commit()
+    
+    def add_orders(self, customer_id, product_id, order_quantity):
 
-cursor.execute("INSERT INTO product (product_name, product_price) VALUES (?, ?)", ('Milk', 10.0))
-cursor.execute("INSERT INTO product (product_name, product_price) VALUES (?, ?)", ('Egg', 20.0))
-cursor.execute("INSERT INTO product (product_name, product_price) VALUES (?, ?)", ('Apple', 30.0))
-cursor.execute("INSERT INTO product (product_name, product_price) VALUES (?, ?)", ('Meat', 20.0))
-cursor.execute("INSERT INTO product (product_name, product_price) VALUES (?, ?)", ('Orange', 15.0))
+        '''
+        to insert values into orders table then commit the changes to the database
+        '''
 
+        self.cursor.execute("INSERT INTO orders (customer_id, product_id,  order_quantity) VALUES (?, ?, ?)", 
+                            (customer_id, product_id, order_quantity))
+        self.conn.commit()
+    
 
-cursor.execute("INSERT INTO orders (customer_id, product_id,  order_quantity) VALUES (?, ?, ?)", (1, 4, 2))
-cursor.execute("INSERT INTO orders (customer_id, product_id,  order_quantity) VALUES (?, ?, ?)", (2, 5, 3))
-cursor.execute("INSERT INTO orders (customer_id, product_id,  order_quantity) VALUES (?, ?, ?)", (3, 4, 1))
+    def get_queries(self):
 
+        '''
+        - Excute the required queries.
+        - Get and print the results.
+        '''
 
-# 6. Commit the changes to the database
-conn.commit()
-
-
-# 7. Query the tables to show the order name  and quantity based on customer name and product.
-cursor.execute('''SELECT customer.first_name, customer.last_name, product.product_name,  orders.order_quantity, product.product_price
+        self.cursor.execute('''SELECT customer.first_name, customer.last_name, product.product_name,  orders.order_quantity, product.product_price
                          FROM customer
                          INNER JOIN orders ON customer.customer_id = orders.customer_id
                          INNER JOIN product ON orders.product_id = product.product_id''')
+        
+
+        rows = self.cursor.fetchall()
+        
+
+        results = []
+        for row in rows:
+            results.append(f'{row[0]} {row[1]} ordered a {row[2]} and the order quantity is  {row[3]} with price: {row[4]}')
+        
+        return results
+    
 
 
-# 5. Get the results
-rows = cursor.fetchall()
+def main():
+
+    # Create a new database and connect to it
+    db = Database('./SQLite/SQLite with Python/database.db')
+
+    # Insert some sample data into the tables.
+    db.add_customers('John ', 'Doe')
+    db.add_customers('Ahmad', 'Soliman')
+    db.add_customers('Muster', 'Mann')
+
+    db.add_products('Milk', 10.0)
+    db.add_products('Egg', 20.0)
+    db.add_products('Apple', 30.0)
+    db.add_products('Meat', 40.0)
+    db.add_products('Orange', 15.0)
+
+    db.add_orders(1, 2, 2)
+    db.add_orders(2, 5, 3)
+    db.add_orders(3, 4, 1)
 
 
-# Print the results
-for row in rows:
-    pprint(f'{row[0]} {row[1]} ordered a {row[2]} and the order quantity is  {row[3]} with price: {row[4]}')
+    # Get the results and store the results into txt file
+    orders = db.get_queries()
+
+    with open('./SQLite/SQLite with Python/output.txt', 'w') as f:
+        for order in orders:
+            f.write(order + '\n')
+            pprint(order)
+
+
+if __name__ == '__main__':
+    main()
